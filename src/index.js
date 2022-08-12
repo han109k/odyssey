a042b59d16bcc5e2211f8f02b8405bcfca292870
Han <han109k@gmail.com>
2022-08-12 12:09:26 +0300

    add port number

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ const resolvers = require('./resolvers');
 
 const TrackAPI = require('./datasources/track-api');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-server.listen().then(() => {
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+
   console.log(`
-    🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at http://localhost:4000
-  `);
-});
+      🚀  Server is running!
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
